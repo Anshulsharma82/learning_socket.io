96218f50ef50ae60e4f9b64f9899338070add1bb
Anshul Sharma <anshulsharma4445@gmail.com>
2025-06-04 18:52:55 +0530

    some style related changes

--- script.js
+++ script.js
@@ -6,45 +6,56 @@ const contentDiv = document.getElementById('contentDiv')
 
 let userName = prompt('What\'s your name?')
 
-while(!userName) {
-    userName = prompt('What\'s your name?')
-}
+userName = userName ? userName : 'Someone'
 
-appendMessage('You Joined!', 'bg-black', 'text-white')
+appendMessage('You Joined!', 'w-[98.5%]', 'bg-black', 'text-white')
 socket.emit('user-connected', userName)
-
 
 socket.on('new-user-connected', (userName) => {
     console.log('broadcast!')
-    appendMessage(`${userName} connected!`, 'bg-green-300')
+    appendMessage(`${userName} connected!`,'w-1/3', 'bg-green-300', 'white', true)
 })
 
 socket.on('boradcast-msg', (obj) => {
     console.log('userName::::::::::::::::::::::::::', userName)
-    appendMessage(`${obj.userName}: ${obj.msg}`)
+    appendMessage(`${obj.userName}: ${obj.msg}`, 'max-w-1/2', 'bg-white', 'black', false)
 })
 
 socket.on('user-disconnected', (userName) => {
-    appendMessage(`${userName} disconnected`, 'bg-red-300')
+    appendMessage(`${userName} disconnected`, 'w-1/3', 'bg-red-300', 'white', true)
 })
 
 form.addEventListener('submit', (e) => {
+    e.preventDefault();
+ 
     if(inputField.value.trim() === '' || !inputField.value) {
         console.log('invalid input')
-        return;
     }
-    e.preventDefault();
-    console.log('button clicked')
-    appendMessage(`You: ${inputField.value}`, 'bg-gray-200', 'white')
-    socket.emit('send-message',inputField.value)
-    inputField.value = ''
+    else {
+        console.log('button clicked')
+        appendMessage(`You: ${inputField.value}`, 'max-w-1/2', 'bg-gray-200', 'white', true)
+        socket.emit('send-message',inputField.value)
+        inputField.value = ''
+    }
 })
 
 
-function appendMessage(msg, backgroundColor='bg-white', color='black') {
+function appendMessage(msg, width, backgroundColor='bg-white', color='black', alignToEnd=false) {
     const newDiv = document.createElement('div')
-    newDiv.innerHTML = msg
-    newDiv.classList.add(backgroundColor, color)
+    const newP = document.createElement('p')
+    newP.innerHTML = msg;
+    newDiv.classList.add(backgroundColor, color, width, 'p-1', 'mx-2', `rounded-md`)
+    newP.classList.add( 'w-full')
+    newDiv.classList.add( 'border-2',)
+    if (alignToEnd) {
+        newDiv.classList.add( 'self-end')
+    }
+    else {
+        newDiv.classList.add('self-start')
+    }
+    console.log('classList::::::::::::::::::::', newDiv.classList)
+    
+    newDiv.appendChild(newP)
     contentDiv.appendChild(newDiv)
 } 
 
@@ -53,4 +64,4 @@ function appendMessage(msg, backgroundColor='bg-white', color='black') {
 //     newDiv.innerHTML = msg
 //     newDiv.classList.add('bg-gray-400', 'text-white')
 //     contentDiv.appendChild(newDiv)
-// } 
+// }  
